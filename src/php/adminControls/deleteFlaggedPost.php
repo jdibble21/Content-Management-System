87f992e321f73357beaa42f428e0a53bdb9a60fb
Sara Bailey <scbailey@bsu.edu>
2021-04-06 13:34:45 -0400

    update deleteFlaggedPost.php

--- src/php/adminControls/deleteFlaggedPost.php
+++ src/php/adminControls/deleteFlaggedPost.php
@@ -1,11 +1,10 @@
 <?php
-include_once '../classes/businessLogic.php';
+include_once '../../../../../classes/businessLogic.php';
 session_start();
 
 $bl = new businessLogic();
 
 $postID = $_POST['postID'];
 $flagID = $_POST['flagID'];
-$userID = $_POST['userID'];
 
-$bl->deleteFlaggedPost($postID, $flagID, $userID);
+$bl->deleteFlaggedOrgPost($postID, $flagID);
